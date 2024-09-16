f05b9d74155321ebc562d0b8468c5959eee33bc0
SirRobert-1 <rviverosgonzalez@gmail.com>
2024-09-15 20:02:03 -0600

    Add initial project files and configurations

---
+++ gervacomercial/src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-blanco">
+      <h1 className="text-7xl text-center text-negro">
+        Gerva Comercial Proximamente
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
