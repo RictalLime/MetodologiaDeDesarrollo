9c5cba8316746acde7daeae732e6054a0f5e50cc
SirRobert-1 <rviverosgonzalez@gmail.com>
2024-09-23 18:19:56 -0600

    Refactor Supabase client initialization and add product insertion
    This commit refactors the Supabase client initialization in the index.jsx file. It replaces the hard-coded Supabase URL and anonymous key with environment variables. Additionally, it adds an insertion query to the 'producto' table, inserting a new product with the specified details.

--- gervacomercial/src/pages/index.jsx
+++ gervacomercial/src/pages/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient("", "")
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
 // const { data, error } = await supabase
 //   .from('talla')
@@ -10,9 +13,16 @@ const supabase = createClient("", "")
 //   .select()
 
 const { data, error } = await supabase
-  .from('producto')
-  .insert({modelo: "prueba", marca: "prueba", precio: 180.0, color: "blanco", tallaid: 5, disponibles: 200})
-  .select()
+  .from("producto")
+  .insert({
+    modelo: "prueba",
+    marca: "prueba",
+    precio: 180.0,
+    color: "blanco",
+    tallaid: 5,
+    disponibles: 200,
+  })
+  .select();
 
 // const { data, error } = await supabase
 //   .from('producto')
