d846a4b62466124df30e437c03522186f3552c2d
AngelSpinosa <133794141+AngelSpinosa@users.noreply.github.com>
2024-10-06 01:12:20 -0600

    Diseño y creacion de crearEmpleado

--- gervacomercial/src/pages/_app.js
+++ gervacomercial/src/pages/_app.js
@@ -1,4 +1,16 @@
 import "@/styles/globals.css";
+import { Roboto } from 'next/font/google';
+import { Playfair_Display } from 'next/font/google';
+
+
+const roboto = Roboto({
+  weight: ['400', '700'], // Especifica los pesos de la fuente
+  subsets: ['latin'], // Subconjunto de la fuente
+});
+const playfair = Playfair_Display({
+  weight: ['400', '900'], // Especifica los pesos de la fuente
+  subsets: ['latin'], // Subconjunto de la fuente
+});
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
