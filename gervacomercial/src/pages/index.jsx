f8a786b3bce299ba0acbf181bd00af598e133aa8
RictalLime <tron@hotmail.com>
2024-10-21 13:44:27 -0600

    n

--- gervacomercial/src/pages/index.jsx
+++ gervacomercial/src/pages/index.jsx
@@ -7,6 +7,24 @@ function Home() {
       <h1 className="text-4xl text-center text-negro">
         Gerva Comercial Proximamente
       </h1>
+      <div className="w-full md:w-1/2 mt-5 p-5">
+        <div className="w-full mt-5">
+          <h1 className="text-negro mb-2">Usuario</h1>
+          <input
+            type="text"
+            placeholder="Usuario"
+            className="border border-negro rounded-md w-full"
+          />
+        </div>
+        <div className="w-full mt-5">
+          <h1 className="text-negro mb-2">Contraseña</h1>
+          <input
+            type="password"
+            placeholder="Contraseña"
+            className="border border-negro rounded-md w-full"
+          />
+        </div>
+      </div>
     </div>
   );
 }
