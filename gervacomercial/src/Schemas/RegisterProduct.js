f649c5343386da90613abf344653a308a65dfeff
SirRobert-1 <rviverosgonzalez@gmail.com>
2024-10-22 16:27:53 -0600

    Refactor RegisterProduct schema to enforce nonempty and letter-only validation for nombre and color fields

--- gervacomercial/src/Schemas/RegisterProduct.js
+++ gervacomercial/src/Schemas/RegisterProduct.js
@@ -1,10 +1,16 @@
 import { z } from "zod";
 
 export const RegisterProduct = z.object({
-  nombre: z.string().nonempty("Escribe el nombre"),
+  nombre: z
+    .string()
+    .nonempty("Escribe el nombre")
+    .regex(/^[a-zA-Z]+$/, "Solo se permiten letras en el nombre"),
   precio: z.number().positive("Escribe el precio"),
   talla: z.number().positive("Escribe la talla"),
   modelo: z.string().nonempty("Escribe el modelo"),
   disponibles: z.number().int().nonnegative("Escribe la cantidad"),
-  color: z.string().nonempty("Escribe el color"),
+  color: z
+    .string()
+    .nonempty("Escribe el color")
+    .regex(/^[a-zA-Z]+$/, "Solo se permiten letras en el color"),
 });
