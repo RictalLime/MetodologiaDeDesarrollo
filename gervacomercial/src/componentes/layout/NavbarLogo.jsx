758b5aa55159916adfbbb71e576df739dcfc7565
SirRobert-1 <rviverosgonzalez@gmail.com>
2024-11-11 17:22:46 -0600

    Add NavbarLogo component and conditionally render in Layout

---
+++ gervacomercial/src/componentes/layout/NavbarLogo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavbarLogo() {
+  return (
+    <div className="w-screen h-20 bg-azul flex justify-center items-center px-4">
+      <div className="flex-grow flex justify-center">
+        <img src="/logoGerva.svg" alt="" className="w-40 h-20" />
+      </div>
+    </div>
+  );
+}
+
+export default NavbarLogo;
