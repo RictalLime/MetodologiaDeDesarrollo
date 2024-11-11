c3a0185c0ce90575611d47b8d8dc320839e1df67
SirRobert-1 <rviverosgonzalez@gmail.com>
2024-11-11 17:25:10 -0600

    Update Navbar and NavbarLogo components to reduce logo size

--- gervacomercial/src/componentes/layout/NavbarLogo.jsx
+++ gervacomercial/src/componentes/layout/NavbarLogo.jsx
@@ -4,7 +4,7 @@ function NavbarLogo() {
   return (
     <div className="w-screen h-20 bg-azul flex justify-center items-center px-4">
       <div className="flex-grow flex justify-center">
-        <img src="/logoGerva.svg" alt="" className="w-40 h-20" />
+        <img src="/logoGerva.svg" alt="" className="w-20 h-10" />
       </div>
     </div>
   );
